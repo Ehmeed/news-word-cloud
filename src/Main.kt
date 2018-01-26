7dffb9ba764ec63d1e33724a71170f742ff5a127
Ehmeed <milanhruban@seznam.cz>
2018-01-26 22:08:47 +0100

    Work for some webs

--- src/Main.kt
+++ src/Main.kt
@@ -9,18 +9,21 @@ fun loadData(): List<Pair<String, List<String>>> {
     println("Loading data..")
     val sites = mutableListOf<Pair<String, List<String>>>()
     sites.add(Pair("https://www.novinky.cz/", listOf("a")))
+    sites.add(Pair("https://www.parlamentnilisty.cz/", listOf("a")))
+    sites.add(Pair("http://forum24.cz/", listOf("a")))
+
     //TODO
     return sites
 }
 
-fun processData(sites: List<Pair<String, List<String>>>) {
+fun processData(sites: List<Pair<String, List<String>>>) : MutableList<String>{
     val words = mutableListOf<String>()
     for(site in sites){
         for(selector in site.second) {
             words.addAll(getHeadlinesBySelector(site.first, selector))
         }
     }
-    words.forEach(::println)
+    return words
 }
 
 fun getHeadlinesBySelector(site: String, selector: String): List<String> {
@@ -30,8 +33,15 @@ fun getHeadlinesBySelector(site: String, selector: String): List<String> {
     for (headline in newsHeadlines) {
         words.addAll(headline.absUrl("href").substringAfterLast("/")
                 .split("-")
-                .filter { !it.contains("mailto") }
                 .filter { it.isNotEmpty()}
+                .filter { it.length > 2 }
+                .filter { !it.contains("www") }
+                .filter { !it.contains("mailto") }
+                .filter { !it.contains("#") }
+                .filter { !it.contains("?") }
+                .filter { !it.contains("novinky", true) }
+                .filter { !it.matches("-?\\d+(\\.\\d+)?".toRegex()) }
+                .map { if(it.contains(".html")) it.substringBefore(".html") else it }
         )
     }
     return words
@@ -41,7 +51,11 @@ fun getHeadlinesBySelector(site: String, selector: String): List<String> {
 fun main(args: Array<String>){
     val sites = loadData()
     println("Processing..")
-    processData(sites)
-
-
+    val words = processData(sites)
+    val occurrenceMap: Map<String, Int> = words.groupingBy { it }
+            .eachCount()
+            .toList()
+            .sortedBy { (_, value) -> value }
+            .toMap()
+    occurrenceMap.forEach(::println)
 }
