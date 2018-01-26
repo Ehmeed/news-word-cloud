a62ed3da4bcf83e05cb5200dd6ca4f2d4fdd8c9e
Ehmeed <milanhruban@seznam.cz>
2018-01-26 21:25:42 +0100

    Added basic scraping and filtering

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,47 @@
-fun main(args: Array<String>){
+
+import org.jsoup.Jsoup
+
+
+
+
+
+fun loadData(): List<Pair<String, List<String>>> {
     println("Loading data..")
-    //loadData()
+    val sites = mutableListOf<Pair<String, List<String>>>()
+    sites.add(Pair("https://www.novinky.cz/", listOf("a")))
+    //TODO
+    return sites
+}
+
+fun processData(sites: List<Pair<String, List<String>>>) {
+    val words = mutableListOf<String>()
+    for(site in sites){
+        for(selector in site.second) {
+            words.addAll(getHeadlinesBySelector(site.first, selector))
+        }
+    }
+    words.forEach(::println)
+}
+
+fun getHeadlinesBySelector(site: String, selector: String): List<String> {
+    val words = mutableListOf<String>()
+    val doc = Jsoup.connect(site).get()
+    val newsHeadlines = doc.select(selector)
+    for (headline in newsHeadlines) {
+        words.addAll(headline.absUrl("href").substringAfterLast("/")
+                .split("-")
+                .filter { !it.contains("mailto") }
+                .filter { it.isNotEmpty()}
+        )
+    }
+    return words
+}
+
+
+fun main(args: Array<String>){
+    val sites = loadData()
     println("Processing..")
-    //processData()
+    processData(sites)
+
 
 }
